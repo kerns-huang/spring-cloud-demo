137db18f1421d732e0f23cb86f89146ff470a9fa
孟津 <huangcb@huipinzhe.com>
2017-03-24 17:38:53 +0800

    m

--- restapi/src/main/java/com/restapi/main/Application.java
+++ restapi/src/main/java/com/restapi/main/Application.java
@@ -2,15 +2,17 @@ package com.restapi.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by kerns on 2017/3/23.
  */
-@Configuration
-@Component
-@EnableAutoConfiguration
+
+@SpringBootApplication
+@EnableEurekaClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
