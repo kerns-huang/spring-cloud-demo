4843b29544be82abce5755687741079963ca42d5
孟津 <huangcb@huipinzhe.com>
2017-03-29 19:19:59 +0800

    m

--- restapi/src/main/java/com/restapi/config/RestApiConfig.java
+++ restapi/src/main/java/com/restapi/config/RestApiConfig.java
@@ -1,34 +1,93 @@
 package com.restapi.config;
 
+import com.google.common.base.Predicate;
+import com.restapi.controller.UserController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.builders.*;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+
+import static com.google.common.base.Predicates.or;
+import static com.google.common.collect.Lists.newArrayList;
+import static springfox.documentation.builders.PathSelectors.ant;
+import static springfox.documentation.builders.PathSelectors.regex;
 
 /**
  * Created by kerns on 2017/3/23.
  */
-@ComponentScan("com.restapi.controller")
 @Configuration
 @EnableSwagger2
-@EnableWebMvc
+@ComponentScan(basePackageClasses = {
+        UserController.class,
+})
 public class RestApiConfig extends WebMvcConfigurationSupport {
 
     @Bean
-    public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().apis(RequestHandlerSelectors.basePackage("com.restapi.controller")).paths(PathSelectors.any()).build();
+    public Docket userApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("user-api")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(userPaths())
+                .build()
+                .securitySchemes(newArrayList(oauth()))
+                .securityContexts(newArrayList(securityContext()));
     }
 
     @Bean
+    SecurityScheme oauth() {
+        return new OAuthBuilder()
+                .name("petstore_auth")
+                .grantTypes(grantTypes())
+                .scopes(scopes())
+                .build();
+    }
+
+    List<AuthorizationScope> scopes() {
+        return newArrayList(
+                new AuthorizationScope("write:pets", "modify pets in your account"),
+                new AuthorizationScope("read:pets", "read your pets"));
+    }
+
+    List<GrantType> grantTypes() {
+        GrantType grantType = new ImplicitGrantBuilder()
+                .loginEndpoint(new LoginEndpoint("http://petstore.swagger.io/api/oauth/dialog"))
+                .build();
+        return newArrayList(grantType);
+    }
+
+    @Bean
+    SecurityContext securityContext() {
+        AuthorizationScope readScope = new AuthorizationScope("read:pets", "read your pets");
+        AuthorizationScope[] scopes = new AuthorizationScope[1];
+        scopes[0] = readScope;
+        SecurityReference securityReference = SecurityReference.builder()
+                .reference("petstore_auth")
+                .scopes(scopes)
+                .build();
+
+        return SecurityContext.builder()
+                .securityReferences(newArrayList(securityReference))
+                .forPaths(ant("/api/pet.*"))
+                .build();
+    }
+
     private ApiInfo apiInfo() {
-        return new ApiInfoBuilder().title("spring 中使用swagger2构建RESTFUL APIS").termsOfServiceUrl("http://blog.csdn.net/he90227").contact("逍遥飞鹤").version("1.1").build();
+        return new ApiInfoBuilder().title("swagger demo").description("swagger 测试demo").version("1.1").build();
+    }
+
+    private Predicate<String> userPaths() {
+        return or(
+                regex("/users/*")
+        );
     }
 }
