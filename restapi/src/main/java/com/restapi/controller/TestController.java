d8dd95490603fcc0358d1820ce525d2bde0b3141
孟津 <huangcb@huipinzhe.com>
2017-04-06 14:10:11 +0800

    m

--- restapi/src/main/java/com/restapi/controller/TestController.java
+++ restapi/src/main/java/com/restapi/controller/TestController.java
@@ -2,10 +2,7 @@ package com.restapi.controller;
 
 import com.restapi.main.Application;
 import com.restapi.vo.User;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -32,7 +29,7 @@ public class TestController {
     @ApiOperation(value = "创建用户", notes = "根据User对象创建用户")
     @RequestMapping(value = "/addUser", method = RequestMethod.POST)
     @ResponseBody
-    public String postUser(@RequestBody User user) {
+    public String postUser(@RequestBody @ApiParam(value = "用户信息") User user) {
         users.put(user.getId(), user);
         return "success";
     }
@@ -40,14 +37,14 @@ public class TestController {
     @ApiOperation(value = "获取用户详细信息", notes = "根据url的id来获取用户详细信息")
     @RequestMapping(value = "/getUser", method = RequestMethod.GET)
     @ResponseBody
-    public User getUser(@PathVariable Long id) {
+    public User getUser(@PathVariable  @ApiParam(value = "用户id") Long id) {
         return users.get(id);
     }
 
     @ApiOperation(value = "更新用户详细信息", notes = "根据url的id来指定更新对象，并根据传过来的user信息来更新用户详细信息")
     @RequestMapping(value = "/updateUser", method = RequestMethod.PUT)
     @ResponseBody
-    public String putUser(@PathVariable Long id, @RequestBody User user) {
+    public String putUser(@PathVariable @ApiParam(value = "用户id") Long id, @RequestBody @ApiParam(value = "用户信息") User user) {
         User u = users.get(id);
         u.setName(user.getName());
         u.setAge(user.getAge());
@@ -58,7 +55,7 @@ public class TestController {
     @ApiOperation(value = "删除用户", notes = "根据url的id来指定删除对象")
     @RequestMapping(value = "/remove", method = RequestMethod.DELETE)
     @ResponseBody
-    public String deleteUser(@PathVariable Long id) {
+    public String deleteUser(@PathVariable @ApiParam(value = "用户id") Long id) {
         users.remove(id);
         return "success";
     }
