d5903cdfd22dae1bae3421d00e6345f465d59055
孟津 <huangcb@huipinzhe.com>
2017-04-12 14:35:05 +0800

    m

--- zuul/src/main/java/com/zuul/main/Application.java
+++ zuul/src/main/java/com/zuul/main/Application.java
@@ -3,10 +3,11 @@ package com.zuul.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@EnableZuulProxy
 @Configuration
 @EnableAutoConfiguration
 @EnableEurekaClient
