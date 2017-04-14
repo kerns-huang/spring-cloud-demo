ef5a81f24b782605bd3bb23dfb23dfcb5ec5ca19
孟津 <huangcb@huipinzhe.com>
2017-04-14 17:52:20 +0800

    m

--- contentCenter/src/main/java/com/content/repository/NewsRepository.java
+++ contentCenter/src/main/java/com/content/repository/NewsRepository.java
@@ -4,9 +4,12 @@ import com.content.pojo.News;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by kerns on 2017/4/12.
  */
 @Repository
 public interface NewsRepository extends ElasticsearchRepository<News, String> {
+    List<News> search(String name);
 }
