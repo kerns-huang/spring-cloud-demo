61dd9d3c0ca378f7be5bf531da2064885196d95b
kerns-huang <yanlei3032@gmail.com>
2017-10-10 13:52:27 +0800

    添加spring boots 测试类

---
+++ restapi/src/test/java/com.restapi.controller/NewTest.java
@@ -0,0 +1,14 @@
+/*
+ * huiguan.com Inc.
+ * Copyright (c) 2017 All Rights Reserved.
+ */
+package com.restapi.controller;
+
+/**
+ * TODO desc
+ *
+ * @author huangchunbo
+ * @since $$Revision:1.0.0, $$Date: 2017/10/10 13:45 $$
+ */
+public class NewTest {
+}
